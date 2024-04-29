f43c9d3008f62637e4cd80daea0b3e26dda72808
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-04-29 12:27:58 +0530

    feat/improve: dashboard section completed and history table added

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import React from "react";
 import CreateTransactionDialog from "./_components/CreateTransactionDialog";
 import Overview from "./_components/Overview";
+import History from "./_components/History";
 
 async function page() {
   const user = await currentUser();
@@ -54,6 +55,7 @@ async function page() {
         </div>
       </div>
       <Overview userSettings={userSettings} />
+      <History userSettings={userSettings} />
     </div>
   );
 }
